3230dbbd946e0932d6e806a520857b8fc8bb43d0
christianASC5 <cnicolas3411@gmail.com>
2019-04-03 12:19:07 -0400

    finalcommit

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ title = document.querySelector("h1");
 buttons =  document.querySelectorAll("button");
 input = document.getElementById("definition");
 
-// buttons[0].addEventListener('click',record(title));
+buttons[0].addEventListener('click',record(title));
 
 function record(itmX){
+    console.log("I ran")
     userText = input.value;
 
     input.value = ""
@@ -15,5 +16,7 @@ function record(itmX){
 
 function display(itmX){
     itmX.innerHTML = userText;
+    console.log(userText);
+    console.log(itmX.innerHTML);
 }
 
