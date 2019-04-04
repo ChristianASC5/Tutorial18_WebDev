ecd9a2a5ee09bcbfd1b7bc47209082a81a8bbb38
Christian Nicolas <cnicolas3411@gmail.com>
2019-04-03 22:51:41 -0400

    submit

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 function initialize() {
     items = document.querySelectorAll("li");
     title = document.querySelector("h1");
-    buttons = document.querySelectorAll("button");
     input = document.getElementById("definition");
 }
 
 
-buttons[0].addEventListener('click', record(title));
+function record(x) {
+     userText = input.value;
 
+     input.value = ""
 
-function record(itmX) {
-    console.log("I ran")
-    userText = input.value;
-
-    input.value = ""
-
-    display(itmX);
+     display(x);
 }
 
-function display(itmX) {
-    console.log(userText);
-    itmX.innerHTML = userText;
-    console.log(itmX);
+function display(x){
+    if(x=="T"){
+        title.innerHTML =  userText;
+    }else{
+        items[x].innerHTML = userText;
+    }
 }
+
+
+
+
+
+
+// RETURN TO THIS LATER
+// TRYING TO PASS PARAMETERS IN HTML
+// function record(itmX) {
+//     console.log("I ran")
+//     userText = input.value;
+
+//     input.value = ""
+
+//     display(itmX);
+// }
+
+// function display(itmX) {
+//     console.log(userText);
+//     itmX.innerHTML = userText;
+//     console.log(itmX);
+// }
